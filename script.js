e583169132ffcf2e313812304660a5e773bdf3c4
akulsingh17 <88528466+akulsingh17@users.noreply.github.com>
2026-08-05 10:44:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Golden Adventure Loaded!");
+const button = document.getElementById("startBtn");
+
+button.addEventListener("click", () => {
+
+    button.innerHTML = "YAY!! 🐶";
+
+    button.style.background = "#8BC34A";
+
+    setTimeout(() => {
+
+        alert("Level 1 coming next! 🎾");
+
+    },600);
+
+});
